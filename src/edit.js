f4a807ba963751f676181bac4995ce921c9f7b50
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-28 12:28:58 +0200

    migrated old code for Editor part, attributes now live in block.json

--- src/edit.js
+++ src/edit.js
@@ -1,3 +1,20 @@
+import {
+  TextControl,
+  Flex,
+  FlexBlock,
+  FlexItem,
+  Button,
+  Icon,
+  PanelBody,
+  PanelRow,
+  ColorPicker,
+} from '@wordpress/components';
+import {
+  InspectorControls,
+  BlockControls,
+  AlignmentToolbar,
+} from '@wordpress/block-editor';
+
 /**
  * Retrieves the translation of text.
  *
@@ -25,15 +42,111 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
-	const blockProps = useBlockProps();
+export default function Edit(props) {
+  const blockProps = useBlockProps();
 
-	return (
-		<p { ...blockProps }>
-			{ __(
-				'Interactivity Quiz – hello from the editor!',
-				'interactivity-quiz'
-			) }
-		</p>
-	);
+  function updateQuestion(value) {
+    props.setAttributes({ question: value });
+  }
+
+  function deleteAnswer(indexToDelete) {
+    const newAnswers = props.attributes.answers.filter((item, index) => {
+      return index != indexToDelete;
+    });
+    props.setAttributes({ answers: newAnswers });
+
+    if (indexToDelete == props.attributes.correctAnswer) {
+      props.setAttributes({ correctAnswer: undefined });
+    }
+  }
+
+  function markAsCorrect(index) {
+    props.setAttributes({ correctAnswer: index });
+  }
+
+  return (
+    <div
+      className='paying-attention-edit-block'
+      style={{ backgroundColor: props.attributes.bgColor }}
+    >
+      <BlockControls>
+        <AlignmentToolbar
+          value={props.attributes.textAlignment}
+          onChange={(e) => {
+            props.setAttributes({ textAlignment: e });
+          }}
+        />
+      </BlockControls>
+      <InspectorControls>
+        <PanelBody title='Background Color' initialOpen={true}>
+          <PanelRow>
+            <ColorPicker
+              color={props.attributes.bgColor}
+              onChangeComplete={(e) => props.setAttributes({ bgColor: e.hex })}
+            />
+          </PanelRow>
+        </PanelBody>
+      </InspectorControls>
+      <TextControl
+        label='Question:'
+        style={{ fontSize: '20px' }}
+        value={props.attributes.question}
+        onChange={updateQuestion}
+      />
+      <p style={{ fontSize: '13px', margin: '20px 0 8px 0' }}>Answers: </p>
+      {props.attributes.answers.map((answer, index) => {
+        return (
+          <Flex>
+            <FlexBlock>
+              <TextControl
+                value={answer}
+                onChange={(newVal) => {
+                  // Doing proper React update here
+                  const newAnswers = props.attributes.answers.concat([]);
+                  newAnswers[index] = newVal;
+                  props.setAttributes({ answers: newAnswers });
+                }}
+                autoFocus={answer == undefined}
+              />
+            </FlexBlock>
+            <FlexItem>
+              <Button
+                onClick={() => {
+                  markAsCorrect(index);
+                }}
+              >
+                <Icon
+                  icon={
+                    props.attributes.correctAnswer == index
+                      ? 'star-filled'
+                      : 'star-empty'
+                  }
+                  className='mark-as-correct'
+                />
+              </Button>
+            </FlexItem>
+            <FlexItem>
+              <Button
+                className='attention-delete'
+                isLink
+                onClick={() => deleteAnswer(index)}
+              >
+                Delete
+              </Button>
+            </FlexItem>
+          </Flex>
+        );
+      })}
+      <Button
+        isPrimary
+        onClick={() => {
+          props.setAttributes({
+            answers: props.attributes.answers.concat([undefined]),
+          });
+        }}
+      >
+        Add Another Answer
+      </Button>
+    </div>
+  );
 }
