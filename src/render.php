4312bdceb0c2e376898fa30ecb00ef8f811a9373
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-28 14:48:35 +0200

    used interactivity API to render data on the frontend. Also moved styles from the other plugin

--- src/render.php
+++ src/render.php
@@ -16,7 +16,19 @@
 <!-- wp data attribute is to gain access to Interactivity API -->
 <!-- In this particualr case we're hooking up to the store named "create-block" that we created in view.js -->
 <!-- Below is example of creating a value in context and pulling it into the text of the Span -->
-<div data-wp-interactive="create-block" data-wp-context='{"clickCount": 0}'>
-	<p>The button below has been clicked <span data-wp-text="context.clickCount"></span> times</p>
-	<button data-wp-on--click="actions.buttonHandler">Click me!</button>
+<!-- Echoing wp_interactivity_data_wp_context populates data-wp-context with whatever we're passing as parameter-->
+<!-- It also conveniently transpiles PHP array into a JS array -->
+<div
+	class="paying-attention-frontend"
+	style="background-color: <?php echo $attributes['bgColor'] ?>;" 
+	data-wp-interactive="create-block"
+	<?php echo wp_interactivity_data_wp_context($attributes) ?>
+	>
+	<p><?php echo $attributes['question'] ?></p>
+	<ul>
+		<!-- data-wp-each sort of starts the loop, and context.item is the reserved way to access one item -->
+		<template data-wp-each="context.answers">
+			<li data-wp-on--click="actions.guessAttempt" data-wp-text="context.item"></li>
+		</template>
+	</ul>
 </div>
